a7d23ef45c3ed17a8000dffa006eb493003f95cb
tjlin653 <tonylin415@gmail.com>
2026-04-16 11:12:33 -0700

    blockbuster js

--- studio1/script.js
+++ studio1/script.js
@@ -14,23 +14,27 @@
         }, 200);
     });
 
-    const sec = document.getElementById('sec');
     const line1 = document.getElementById('line1');
     const line2 = document.getElementById('line2');
-    const intervalID = setInterval(checkTime, 1000);
     const poem = {
-            start: [1, 3],
-            stop: [5, 7],
-            line: [line1, line2]
-        }
+        start: [1, 3],
+        stop: [5, 7],
+        line: [line1, line2]
+    }
 
-            function checkTime() {
-                for (let i=0; i<poem.start.length; i++) {
-                    if (poem.start[i] < myVid.currentTime && myVid.currentTime < poem.stop[i]) {
-                        poem.line[i].className = 'showing';
-                    } else {
-                        poem.line[i].className = 'hidden';
-                    }
-                }
+    const intervalID = setInterval(checkTime, 1000);
+    function checkTime() {
+        for (let i=0; i<poem.start.length; i++) {
+            if (poem.start[i] < myVid.currentTime && myVid.currentTime < poem.stop[i]) {
+                poem.line[i].className = 'showing';
+            } else {
+                poem.line[i].className = 'hidden';
             }
+        }
+    }
+
+    const loading = document.querySelector('.fa-sun');
+    myVid.addEventListener('loadeddata', function() {
+        loading.style.display = 'none';
+    })
 })();
