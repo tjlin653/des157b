1be10c42b60a884ffe5d8f1cf692af8c2e8a3719
tjlin653 <tonylin415@gmail.com>
2026-04-16 10:36:30 -0700

    bluckbuster js

---
+++ studio1/script.js
@@ -0,0 +1,36 @@
+(function (){
+    'use strict';
+
+    const myVid = document.getElementById('myVid');
+
+    let timeout;
+    myVid.pause();
+
+    document.addEventListener('mousemove', function() {
+        myVid.play();
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            myVid.pause();
+        }, 200);
+    });
+
+    const sec = document.getElementById('sec');
+    const line1 = document.getElementById('line1');
+    const line2 = document.getElementById('line2');
+    const intervalID = setInterval(checkTime, 1000);
+    const poem = {
+            start: [1, 3],
+            stop: [5, 7],
+            line: [line1, line2]
+        }
+
+            function checkTime() {
+                for (let i=0; i<poem.start.length; i++) {
+                    if (poem.start[i] < myVid.currentTime && myVid.currentTime < poem.stop[i]) {
+                        poem.line[i].className = 'showing';
+                    } else {
+                        poem.line[i].className = 'hidden';
+                    }
+                }
+            }
+})();
